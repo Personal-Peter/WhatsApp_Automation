54105376193b0df7f6161f133671b3dcf0072945
Hello.Ship.Code <hello.ship.code@gmail.com>
2023-04-03 17:51:51 -0400

    Scheduler_Text 📄

---
+++ scheduler_text.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import time
+import pywhatkit as whatsapp
+
+
+from scheduler import Scheduler
+import scheduler.trigger as trigger # Use to trigger specfic days ("Monday", "Tuesday")
+
+phone_number = input('Enter your phone Number\n')
+
+def foo():
+    whatsapp.sendwhatmsg_to_group_instantly(phone_number,"Hay.",15,True,2)
+    #Opens whatsapp web & sends the Message to the recipent.
+    print("testing")
+
+schedule = Scheduler()
+
+# schedule.minutely(dt.time(second=15), foo)
+
+schedule.daily(dt.time(hour=14, minute=20), foo) #Schedules the message daily at 2:20pm
+
+
+
+while True:
+    schedule.exec_jobs()
+    time.sleep(1)
---
+++ text.py
@@ -0,0 +1,3 @@
+import pywhatkit as whatsapp
+
+whatsapp.sendwhatmsg("+15162733303","Hay.",13,47,15,True,2) # Check Read me file.
